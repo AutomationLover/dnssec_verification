c937634aa037e60ec0a3efbc637768aa94e06bdd
William Wang <wwang2@atlassian.com>
2022-10-26 19:44:26 +1100

    update request

--- module/zone.py
+++ module/zone.py
@@ -4,22 +4,40 @@ g_zones = dict()  # {domain: class zone}
 g_sent_queries = []
 
 
+def get_sent_queries():
+    return g_sent_queries
+
+
 class Request:
     _request_func = None
     
     @classmethod
-    def set_query_func(cls, query_func):
+    def set_query_func(cls, query_func):  # enable test with local_show_file, avoid interact with ns.
         cls._request_func = query_func
 
     @classmethod
     def query(cls, *args, **kwargs):
-        # here to log command called, and time etc.
-        g_sent_queries.append([args, kwargs])
-        
+        # before query record, to record command called in g_sent_queries.
+        def log_before_query():
+            items = []
+            if args:
+                parameters = ['domain', 'dns_record', 'query_to']
+                for i,arg in enumerate(args):
+                    item = f'{parameters[i]}={str(arg)}'
+                    items.append(item)
+            if kwargs:
+                for k, v in kwargs.items():
+                    item = f'{k}={str(v)}'
+                    items.append(item)
+            parameter_str = ','.join(items)
+            log_string = f'query({parameter_str})'
+            g_sent_queries.append(log_string)
+
+        log_before_query()
         result = cls._request_func(*args, **kwargs)
         return result
         
-
+    
 class FailQueryNS(Exception):
     pass
 
